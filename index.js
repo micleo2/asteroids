687e01219c06782a41689d4c38deedeb7e43517d
Michael Leon <programmermike1@gmail.com>
2016-04-18 20:34:56 -0500

    Init repo

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  socket.on("playerUpdate", function (player) {
+    socket.broadcast.emit('playerUpdate', {id: socket.id, player: player});
+  });
+});
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
+});
