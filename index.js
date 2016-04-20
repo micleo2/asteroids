59c42feaae751cd482b39d598b13c39dbf611a71
Michael Leon <programmermike1@gmail.com>
2016-04-19 19:50:05 -0500

    Client handles remote players' disconnect and bullet collison

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var socketPool = {};
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
+  socketPool[socket.id] = socket;
   socket.on("playerUpdate", function (player) {
     socket.broadcast.emit('playerUpdate', {id: socket.id, player: player});
   });
   socket.on("fireBullet", function (bulObj) {
+    bulObj.id = socket.id;
     socket.broadcast.emit('fireBullet', bulObj);
+  });
+  socket.on("bulletHit", function (bulObj) {
+    socketPool[bulObj.id].emit("bulletSuccess", bulObj);
+  })
+
+  socket.on("disconnect", function () {
+    io.emit("playerLeft", {id: socket.id});
+    delete socketPool[socket.id];
   });
 });
 
